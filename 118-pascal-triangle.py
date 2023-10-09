f566210c0927db4d51a322538e82cca08578b7a9
Muhammad-Jafri <114304752+Muhammad-Jafri@users.noreply.github.com>
2023-10-09 13:32:59 +0500

    added new problems

---
+++ 118-pascal-triangle.py
@@ -0,0 +1,17 @@
+# given n = numRows(number of rows of a pascal triangle)
+# output = pascal triangle upto n
+
+# n = 3
+# output = [[1],[1,1],[1,2,1]]
+
+# you should be able to do this using dp
+
+
+
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        
